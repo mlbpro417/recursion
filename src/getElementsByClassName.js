73f867d0ab0b04d091df999757a57090b5f05774
mlbpro417 <mlbpro417@gmail.com>
2018-01-07 17:36:13 -0800

    finished stringify and getElementByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+  var getElementsByClassName = function (className, that) {
+  if(!that) that = document;
+  that = that.childNodes
+  var elements = [];
+  // loop over all child nodes
+  if(that.length > 0){
+  // if that node has className, add it to elements
+    for(var i = 0; i < that.length; i++){
+      var classes = that[i].classList;
+      for(key in classes){
+        if(classes[key] == className) {
+          elements.push(that[i]);
+          break;
+        }
+      }
+  // now repeat for each node nested in current child node
+      var nestedElements = getElementsByClassName(className,that[i]);
+  // and add non-undefined elements one at a time to elements
+      for(var j = 0; j < nestedElements.length; j++) if(nestedElements[j]) elements.push(nestedElements[j]);
+    }
+  }
+  return elements
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,45 @@
 
 // but you don't so you're going to write it from scratch:
 
+var objectStringify = function(obj) {
+
+  var  strings = [];
+
+  _.each(obj, function(val, key){
+    if (_.isUndefined(val) || _.isFunction(val)) {
+      return;
+    }
+    strings.push( stringifyJSON(key) + ":" + stringifyJSON(val) );
+  });
+
+  return '{' + strings.join(',') + '}';
+};
+
+var arrayStringify = function(arr) {
+  return "[" + _.map(arr, stringifyJSON).join(",") + "]";
+};
+
 var stringifyJSON = function(obj) {
   // your code goes here
+  
+  // objects stringify
+  if ({}.toString.call(obj) === "[object Object]") {
+    return objectStringify(obj);
+
+  // arrays
+  } else if (Array.isArray(obj)) {
+    return arrayStringify(obj);
+  // strings
+  } else if (typeof obj === "string"){
+    return '"' + obj + '"';
+  // undefined things
+  } else if (typeof obj === "function" || obj === undefined) {
+    return;
+  // null
+  } else if (obj === null) {
+    return 'null';
+  // numbers, booleans
+  } else {
+    return obj.toString();
+  }
 };
